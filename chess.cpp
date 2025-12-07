ba3df201ea363ea10bc74f9dd03782f42b11f578
Jay Zhong <jay.zhong99@gmail.com>
2025-12-07 19:28:24 +1100

    Printed chessboard

--- chess.cpp
+++ chess.cpp
@@ -43,17 +43,50 @@ class Game {
     }
 };
 
+void draw_board(Game& game, sf::RenderWindow& window);
+
 int main() {
     Game game {};
-    sf::Window window(sf::VideoMode({1000, 800}), "Chess");
+    sf::RenderWindow window(sf::VideoMode({1000, 800}), "Chess");
+    sf::RectangleShape board({760, 760});
+   
     while (window.isOpen()) {
+        
         while (const std::optional event = window.pollEvent()) {
             if (event->is<sf::Event::Closed>()) {
                 window.close();
             }
         }
+        draw_board(game, window);
+        window.clear(sf::Color::Red);
+
     }
 
+}
+
+void draw_board(Game& game, sf::RenderWindow& window) {
+    float x_offset {}, y_offset {};
+    
+
+    
+    
+    
+    for (int i { 0 }; i < 8; i++) {
+        for (int j { 0 }; j < 8; j++) {
+            sf::RectangleShape cell({760 / 8, 760 / 8});
+            x_offset = 120 + i * (760 / 8);
+            y_offset = 20 + j * (760 / 8);
+            cell.setPosition({x_offset, y_offset});
+            if (game.board[i][j].colour == "brown") {
+                cell.setFillColor(sf::Color::Black);
+            } else {
+                cell.setFillColor(sf::Color::White);
+            }
+            window.draw(cell);
+        }
+    }
+
+    window.display();
 }
 
 
