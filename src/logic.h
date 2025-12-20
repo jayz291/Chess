70eaa386964f89d48b8b0410b946a368ede12afc
Jay Zhong <jay.zhong99@gmail.com>
2025-12-20 12:01:48 +1100

    this should have been added the last commit

--- src/logic.h
+++ src/logic.h
@@ -11,6 +11,11 @@ uint64_t find_rook_attacks(int square, Bitboards& bitboards);
 uint64_t find_bishop_attacks(int square, Bitboards& bitboards);
 bool determine_square_validity(int square, int direction);
 
+int create_encoded_move(int from, int to, int flags);
+int get_flags(Encoded_move move);
+int get_from(Encoded_move move);
+int get_to(Encoded_move move);
+
 int validate_move(Game& game, Bitboards& bitboards, Chessboard& board, Move& move, bool only_checking_checks = false);
 int validate_move_pawn(Game& game, Bitboards& bitboards, Chessboard& board, Move& move);
 int validate_move_knight(Bitboards& bitboards, Chessboard& board, Move& move);
@@ -22,6 +27,7 @@ int check_checks(Game& game, Bitboards& bitboard_copy, Chessboard& copy, Move& m
 void evaluate_king_checks(Game& game);
 int test_castling(Game& game, Bitboards& bitboard_copy, Chessboard& copy, Move& move);
 int validate_en_passant(Game& game, Chessboard& board, Move& move);
+int is_square_attacked(Bitboards& bitboard_copy, int square, int turn);
 
 void make_game_move(Game& game, Bitboards& bitboards, Chessboard& board, int result, Move move);
 void make_test_move(Game& game, Bitboards& bitboards, Chessboard& board, int result, Move& move);
@@ -30,6 +36,7 @@ void undo_game_move(Game& game);
 void undo_test_move(Game& game, Chessboard& board, Move& prev_move);
 void undo_move(Game& game, Bitboards& bitboards, Chessboard& board, Move& prev_move, int turn);
 void handle_pawn_promotion(Game& game, Bitboards& bitboards, Chessboard& board, Move& move);
+void switch_move(Move& move);
 
 std::vector<Move> determine_possible_moves(Game& game, Chessboard& board, int turn, bool CPU = false);
 int determine_repetition(Game& game);
